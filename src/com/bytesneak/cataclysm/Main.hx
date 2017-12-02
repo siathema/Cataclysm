58554d0629573808b586c573cce45de6a8c3a416
Matthias Linford <nate.holaday@gmail.com>
2017-12-01 19:46:41 -0800

    initial commit

---
+++ src/com/bytesneak/cataclysm/Main.hx
@@ -0,0 +1,21 @@
+package com.bytesneak.cataclysm;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author Matthias Linfrod
+ */
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+
+}
